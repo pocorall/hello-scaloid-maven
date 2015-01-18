abd1413b5f69d85135f46fcbeee5f82a51a7e61f
Sung-Ho Lee <pocorall@gmail.com>
2015-01-18 12:05:17 +0900

    Update example

--- src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
+++ src/main/scala/org/scaloid/hello/HelloScaloidActivity.scala
@@ -9,16 +9,18 @@ class HelloScaloidActivity extends SActivity {
     contentView = new SVerticalLayout {
       style {
         case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
-        case t: STextView => t.textSize(10 dip)
-        case v => v.backgroundColor(Color.YELLOW)
+        case t: STextView => t textSize 10.dip
+        case e: SEditText => e.backgroundColor(Color.YELLOW)
       }
-
       STextView("I am 10 dip tall")
       STextView("Me too")
-      STextView("I am taller than you").textSize(15 dip) // overriding
-      SEditText("Yellow input field")
-      SButton(R.string.red)
-    }.padding(20 dip)
+      STextView("I am 15 dip tall") textSize 15.dip // overriding
+	  this += new SLinearLayout {
+	    STextView("Button: ")
+        SButton(R.string.red)
+	  }.wrap
+	  SEditText("Yellow input field fills the space").fill
+    } padding 20.dip
   }
 
 }
